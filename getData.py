df3ad97c6cee1424708dc88e64148b1700a0dfbc
Paul Wang <puyuwang@outlook.com>
2023-01-30 18:26:58 +0000

    minor update for further work

---
+++ getData.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+def getData(self):
+    return 
+
+
+__version__ = "0.1"
+
+if __name__ == "__main__":
+    print("This is a module, not a script. Please import it.")
--- makeLD.py
+++ makeLD.py
@@ -1,20 +1,40 @@
+# -*- coding: UTF-8 -*-
 from rdflib import Graph, URIRef, Literal, BNode
-from rdflib.namespace import FOAF, RDF
-
+from rdflib.namespace import FOAF, RDF, RDFS, XSD, DCMI
+import uuid
+class entry():
+    # class for the entries of Readings
+    # read_day: day of reading
+    # title: title of the paper
+    # link: link of the paper
+    # authors: authors of the paper
+    # comments: comments on the paper
+    # date: date of the paper
+    # tags: tags of the paper
+    def __init__(self, read_day, title, link, authors, comments,date, tags):
+        self.read_day = read_day
+        self.title = title
+        self.link = link
+        self.authors = authors
+        self.comments = comments
+        self.date = date
+        self.tags = tags
+    
 g = Graph()
 g.bind("foaf", FOAF)
+g.bind("rdf", RDF) 
+g.bind("rdfs", RDFS) 
+g.bind("xsd", XSD) 
+g.bind("dcmi", DCMI)
+
+book = URIRef("http://example.org/people/Bob")
+
 
 bob = URIRef("http://example.org/people/Bob")
-linda = BNode()  # a GUID is generated
+linda = URIRef("http://example.org/people/Linda")
+testClass = URIRef("http://example.org/class/testClass")
 
-name = Literal("Bob")
-age = Literal(24)
 
-g.add((bob, RDF.type, FOAF.Person))
-g.add((bob, FOAF.name, name))
-g.add((bob, FOAF.age, age))
-g.add((bob, FOAF.knows, linda))
-g.add((linda, RDF.type, FOAF.Person))
-g.add((linda, FOAF.name, Literal("Linda")))
 
-print(g.serialize(destination="./RDFs/test.ttl"))
+if __name__ == "__main__":
+    g.serialize(destination="./RDFs/test.ttl")
