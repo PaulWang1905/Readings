a61335111d61547d4e4cd78c7b06e145660bae0d
Puyu Wang <puyuwang@outlook.com>
2023-02-03 01:46:44 +0000

    Update makeLD.py

--- makeLD.py
+++ makeLD.py
@@ -74,3 +74,4 @@ def makeLD(self):
 if __name__ == "__main__":
     g = makeLD(entries)
     g.serialize(destination="./RDFs/readings.ttl", format='turtle')
+    g.serialize(destination="./RDFs/readings.json", format='json-ld')
